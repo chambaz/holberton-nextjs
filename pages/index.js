3f9277ee9efc2ce99a17df0f873281293457bc9e
Adam Chambers <adammchambers@gmail.com>
2020-07-25 21:22:41 -0400

    Switch class to className and add key to posts loop

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,9 @@ export default function Home() {
     <main className="container mx-auto px-4">
       <section className="pb-16 mb-8">
         <h2 className="text-4xl font-semibold mt-8 mb-8">Holberton feed</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
-          {posts.map((post) => {
-            return <Post data={post} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+          {posts.map((post, i) => {
+            return <Post key={i} data={post} />
           })}
         </div>
       </section>
